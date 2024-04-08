4a1a61d12cbaf66cc6003478d3cf5be30d9212e0
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-04-08 08:53:51 -0600

    Finalizacion de la Capa Negocio

---
+++ H20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,17 @@
+﻿
+using H20240408.AccesoADatos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace H20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddScoped<PersonaHBL>();
+            return services;
+        }
+    }
+}
---
+++ H20240408.LogicaDeNegocio/PersonaHBL.cs
@@ -0,0 +1,38 @@
+﻿using H20240408.AccesoADatos;
+
+
+namespace H20240408.LogicaDeNegocio
+{
+    public class PersonaABL
+    {
+        readonly PersonaHDAL _personaHDAL;
+        public PersonaABL(PersonaHDAL personaHDAL)
+        {
+            _personaHDAL = personaHDAL;
+        }
+        public async Task<int> Crear(PersonaH personaH)
+        {
+            return await _personaHDAL.Crear(personaH);
+        }
+        public async Task<int> Modificar(PersonaH personaH)
+        {
+            return await _personaHDAL.Modificar(personaH);
+        }
+        public async Task<int> Eliminar(PersonaH personaH)
+        {
+            return await _personaHDAL.Eliminar(personaH);
+        }
+        public async Task<PersonaH> ObtenerPorId(PersonaH personaH)
+        {
+            return await _personaHDAL.ObtenerPorId(personaH);
+        }
+        public async Task<List<PersonaH>> ObtenerTodos()
+        {
+            return await _personaHDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonaH>> Buscar(PersonaH personaH)
+        {
+            return await _personaHDAL.Buscar(personaH);
+        }
+    }
+}
